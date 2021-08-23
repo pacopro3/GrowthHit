1815921200814875db9517dca1b5db231b04c4a9
Paco Pro <45984546+pacopro3@users.noreply.github.com>
2021-08-23 16:37:25 -0500

    Update HitApplicationTests.java
    Se realizan las pruebas correspondientes para obtener el 50% de coding coverage.

--- hit/src/test/java/com/example/hit/HitApplicationTests.java
+++ hit/src/test/java/com/example/hit/HitApplicationTests.java
@@ -1,39 +1,80 @@
 package com.example.hit;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.io.IOException;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import org.apache.commons.lang.RandomStringUtils;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.*;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.*;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
+import com.example.hit.entity.HitEntity;
+import com.example.hit.entity.HitRespuesta;
+import com.example.hit.interf.HitInterface;
+import com.example.hit.rabbitmq.consumer.ReceiveMessageHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @SpringBootTest
 class HitApplicationTests {
 
+	@Autowired
+	HitInterface interface1;
+
+
+
 	@Test
-	void contextLoads() {
+	public void TestEntity(){
+		HitEntity entity = new HitEntity();
+		entity.setId(1);
+		entity.setHits("test");
+		entity.setCount(3);
+		assertEquals(1, entity.getId());
+		assertEquals("test", entity.getHits());
+		assertEquals(3, entity.getCount());
 	}
 
 	@Test
-	public void TestCorrecto()  throws ClientProtocolException, IOException {
- 
-		// Given
-		String jsonMimeType = "application/json";
-		String name = RandomStringUtils.randomAlphabetic( 5 );
-		HttpUriRequest request = new HttpGet( "http://localhost:8080/hit/" + name);
-		request.addHeader("accept",jsonMimeType);
-		// When
-		HttpResponse httpResponse = HttpClientBuilder.create().build().execute( request );
-	
-		// Then
-		
-		assertThat(httpResponse.getStatusLine().getStatusCode(),equalTo(HttpStatus.SC_ACCEPTED));
+	public void TestRespuesta(){
+		HitRespuesta respuesta = new HitRespuesta();
+		respuesta.setKey("test1");
+		respuesta.setHits(5);
+		assertEquals("test1", respuesta.getKey());
+		assertEquals(5, respuesta.getHits());
+	}
+
+	@Test
+	public void ReceiveMessageHandlerTest(){
+		ReceiveMessageHandler msghandler;
+		msghandler = new ReceiveMessageHandler(interface1);
+		String name = "test2";
+		assertEquals(1, msghandler.handleMessage(name));
+		assertEquals(2, msghandler.handleMessage(name));
+	}
+
+	@Test
+	public void ReceiveMessageHandlerTest1(){
+		ReceiveMessageHandler msghandler;
+		msghandler = new ReceiveMessageHandler(interface1);
+		String jsonbody = msghandler.fetchAll(1);
+		ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        String arrayToJson = "";
+		List<HitRespuesta> lista = new ArrayList<HitRespuesta>();
+        HitRespuesta temp = new HitRespuesta();
+		temp.setKey("test2");
+		temp.setHits(2);
+		lista.add(temp);
+		try {
+            arrayToJson = objectMapper.writeValueAsString(lista);
+			assertEquals(arrayToJson,jsonbody);
+	} catch (JsonProcessingException e) {
+		e.printStackTrace();
+		assertNotEquals(arrayToJson, jsonbody);
+	}
 	}
 }
-
