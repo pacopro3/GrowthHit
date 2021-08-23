a3599d91f431e184860936398e56bb7b29ff9efa
Paco Pro <45984546+pacopro3@users.noreply.github.com>
2021-08-23 01:08:46 -0500

    Update HitApplicationTests.java
    Se genera el test pero no se ejecuta el programa.

--- hit/src/test/java/com/example/hit/HitApplicationTests.java
+++ hit/src/test/java/com/example/hit/HitApplicationTests.java
@@ -1,14 +1,39 @@
 package com.example.hit;
 
 import org.junit.jupiter.api.Test;
-
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+
+import org.apache.commons.lang.RandomStringUtils;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.*;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 @SpringBootTest
 class HitApplicationTests {
 
 	@Test
-	public void TestSendMessgeController() {
-		}
+	void contextLoads() {
+	}
 
+	@Test
+	public void TestCorrecto()  throws ClientProtocolException, IOException {
+ 
+		// Given
+		String jsonMimeType = "application/json";
+		String name = RandomStringUtils.randomAlphabetic( 5 );
+		HttpUriRequest request = new HttpGet( "http://localhost:8080/hit/" + name);
+		request.addHeader("accept",jsonMimeType);
+		// When
+		HttpResponse httpResponse = HttpClientBuilder.create().build().execute( request );
+	
+		// Then
+		
+		assertThat(httpResponse.getStatusLine().getStatusCode(),equalTo(HttpStatus.SC_ACCEPTED));
+	}
 }
+
